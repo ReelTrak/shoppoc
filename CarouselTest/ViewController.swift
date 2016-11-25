eaba79c06df66fc0577d93e013980ad61f860422
Peter Bødskov <pb@reeltrak.com>
2016-11-25 14:29:26 +0100

    table view is go

--- CarouselTest/ViewController.swift
+++ CarouselTest/ViewController.swift
@@ -12,15 +12,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var shopViewContainer: UIView!
+    @IBOutlet weak var eduViewContainer: UIView!
+    @IBOutlet weak var viewSelectorSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        showSelectedView()
+    }
+    
+    func showSelectedView() {
+        if viewSelectorSegmentedControl.selectedSegmentIndex == 0 {
+            shopViewContainer.isHidden = false
+            eduViewContainer.isHidden = true
+        } else {
+            shopViewContainer.isHidden = true
+            eduViewContainer.isHidden = false
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }    
+    }
+    
+    @IBAction func didSelectView(_ sender: UISegmentedControl) {
+        showSelectedView()
+    }
 }
 
